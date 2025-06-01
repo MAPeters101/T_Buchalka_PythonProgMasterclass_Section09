99c25e9977fa3c2accdba30cdd67eec7b1354fee
Mark Peters <mapeters101@earthlink.net>
2025-06-01 16:31:49 -0400

    Complete work on lesson298 from 6:10.

--- lesson298.py
+++ lesson298.py
@@ -8,6 +8,7 @@
 # turtle.done()
 
 import turtle
+import math
 
 
 def square(length: int) -> None:
@@ -16,8 +17,21 @@ def square(length: int) -> None:
         turtle.forward(length)
         turtle.right(90)
 
-for s in range(72):
-    square(120)
-    turtle.left(5)
+
+def encircled_square(length: int) -> None:
+    """Draws a square of length 'length',
+    then encloses it in a circle.
+    """
+    square(length)
+    angle = math.radians(45)
+    radius = length * math.cos(angle)
+    turtle.right(135)
+    turtle.circle(radius)
+
+
+encircled_square(300)
+# for s in range(72):
+#     square(120)
+#     turtle.left(5)
 
 turtle.done()
