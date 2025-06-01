1bba483a2c9f5cf70d1f43684f6627f954de26f7
Mark Peters <mapeters101@earthlink.net>
2025-06-01 16:21:32 -0400

    Work on lesson298 from 3:20 to 5:00.

--- lesson298.py
+++ lesson298.py
@@ -1,10 +1,20 @@
+# import turtle
+#
+# turtle.pendown()
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.done()
+
 import turtle
 
-turtle.pendown()
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
+
+def square(length: int) -> None:
+    """Draws a square of length 'length'"""
+    for side in range(4):
+        turtle.forward(length)
+        turtle.right(90)
+
+square(120)
 turtle.done()
-
-
